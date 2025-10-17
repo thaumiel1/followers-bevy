ce38a77fca0ddae28aff220593cc5a6c06620f8c
thaumiel1 <williamkane562@gmail.com>
2025-10-17 16:34:01 +0100

    Continue working on this

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use rand::prelude::*;
 struct Movement {
     velocity: f32,
     acceleration: f32,  // Rate of gaining speed
-    inertia: f32,       // Energy held by the follower
     accelerating: bool, // True if speeding up, false if slowing down
 }
 
@@ -45,8 +44,7 @@ fn setup(
         Transform::from_xyz(100.0, 1.0, 1.0),
         Movement {
             velocity: 0.0,
-            acceleration: 1.0,
-            inertia: 1.0,
+            acceleration: 10.0,
             accelerating: true,
         },
         RotateToCursor { turn_speed: 1.5 },
@@ -54,15 +52,20 @@ fn setup(
 }
 
 fn update_movement(
-    mut query: Query<(&mut Movement, &Transform), With<Follower>>,
-    camera_query: Query<(&Camera, &GlobalTransform)>,
-    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut query: Query<(&mut Movement, &mut Transform), With<Follower>>,
     time: Res<Time>,
 ) {
     for entity in query.iter_mut().collect::<Vec<_>>() {
         let (mut movement, mut transform) = entity;
-        let direction = transform.local_x();
-        transform.translation += direction * *movement.velocity * time.delta_secs();
+        let direction = transform.local_y();
+        movement.velocity += movement.acceleration * time.delta_secs();
+        if !movement.accelerating {
+            movement.velocity = -movement.acceleration * time.delta_secs();
+            if movement.velocity < 0.0 {
+                movement.velocity = 0.0;
+            }
+        }
+        transform.translation += direction * movement.velocity * time.delta_secs();
     }
 }
 
@@ -85,24 +88,26 @@ fn get_cursor(
         return None;
     };
     //println!("cursor position: {:?}", cursor_world_pos);
-    return Some(cursor_world_pos);
+    Some(cursor_world_pos)
 }
 
 fn rotate_to_cursor(
     time: Res<Time>,
-    mut query: Query<(&RotateToCursor, &mut Transform), With<Follower>>,
+    mut query: Query<(&RotateToCursor, &mut Transform, &mut Movement), With<Follower>>,
     camera_query: Query<(&Camera, &GlobalTransform)>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let cursor_translation = get_cursor(camera_query, window_query).unwrap_or(Vec2::ZERO);
 
-    for (config, mut follower_transform) in &mut query {
+    for (config, mut follower_transform, mut movement) in &mut query {
         let follower_forward = (follower_transform.rotation * Vec3::Y).xy();
         let to_player = (cursor_translation - follower_transform.translation.xy()).normalize();
         let forward_dot_player = follower_forward.dot(to_player);
-        if (forward_dot_player - 1.0).abs() < f32::EPSILON {
+        if forward_dot_player.abs() < 0.19209290e-07_f32 {
+            movement.accelerating = true;
             continue;
         }
+        movement.accelerating = false;
         let follower_right = (follower_transform.rotation * Vec3::X).xy();
         let right_dot_player = follower_right.dot(to_player);
         let rotation_sign = -f32::copysign(1.0, right_dot_player);
